e6caee2057c83a8d3a85d9b90676e628daf9e01b
Asifabc555 <107749289+Asifabc555@users.noreply.github.com>
2022-10-18 18:33:45 +0530

    Update config.py

--- Cutiepii_Robot/config.py
+++ Cutiepii_Robot/config.py
@@ -4,7 +4,7 @@ import json
 import os
 
 def get_user_list(config, key):
-    with open("{}/MakimaProBot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Cutiepii_Robot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
